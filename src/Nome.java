539f09d01fd39553978a7945c783701a3c5ec0b3
Yasmin <yasmin.aparecida.cordeiro@escola.pr.gov.br>
2024-09-30 11:37:40 -0300

    iniciando os arquivos

---
+++ src/Nome.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Nome {
+    public static void main(String[] args) {
+            Scanner leitura = new Scanner(System.in);
+            System.out.println("Digite seu nome: ");
+            String nome = leitura.nextLine();
+        System.out.println(nome);
+
+    }
+}
